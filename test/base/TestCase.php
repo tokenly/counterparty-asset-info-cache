bb4a245fc8de0d5988a021cf1313a9e1bd82de5e
Devon Weller <dweller@devonweller.com>
2015-01-15 22:28:52 -0600

    removed unused comment

--- test/base/TestCase.php
+++ test/base/TestCase.php
@@ -49,7 +49,6 @@ class TestCase extends Orchestra\Testbench\TestCase {
      */
     protected function getPackageProviders($app)
     {
-        // return ['Tokenly\XChainListener\XChainListenerServiceProvider'];
         return [];
     }
 
