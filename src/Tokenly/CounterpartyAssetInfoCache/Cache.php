9b44610bc4984dd208e5e7128ae3864112aa4480
Devon Weller <devon@tokenly.co>
2016-09-14 17:14:13 -0500

    added getMultiple method

--- src/Tokenly/CounterpartyAssetInfoCache/Cache.php
+++ src/Tokenly/CounterpartyAssetInfoCache/Cache.php
@@ -31,6 +31,32 @@ class Cache
         return $info;
     }
 
+    public function getMultiple($asset_names) {
+        $asset_results_by_name = [];
+        $assets_to_load = [];
+        foreach($asset_names as $asset_name) {
+            $cached = $this->getFromCache($asset_name);
+            if ($cached) {
+                $asset_results_by_name[$asset_name] = $cached;
+                continue;
+            }
+            $asset_results_by_name[$asset_name] = [];
+            $assets_to_load[] = $asset_name;
+        }
+
+        if ($assets_to_load) {
+            $xcpd_results = $this->loadFromXCPD($assets_to_load);
+            if ($xcpd_results) {
+                foreach($xcpd_results as $xcpd_result) {
+                    $asset_results_by_name[$xcpd_result['asset']] = $xcpd_result;
+                    $this->laravel_cache->forever($xcpd_result['asset'], $xcpd_result);
+                }
+            }
+        }
+
+        return array_values($asset_results_by_name);
+    }
+
     public function isDivisible($asset_name) {
         $info = $this->get($asset_name);
         if (!isset($info['divisible'])) { return null; }
@@ -49,31 +75,45 @@ class Cache
         $this->laravel_cache->forget($asset_name);
     }
 
-    protected function loadFromXCPD($asset_name) {
-        $assets = $this->xcpd_client->get_asset_info(['assets' => [$asset_name]]);
+    protected function loadFromXCPD($asset_names) {
+        $requested_single_asset = false;
+        if (!is_array($asset_names)) {
+            $asset_names = [$asset_names];
+            $requested_single_asset = true;
+        }
+
+        $assets = $this->xcpd_client->get_asset_info(['assets' => $asset_names]);
         if (!$assets) {
             // this could be a non-valid asset
             return [];
         }
-        $asset_info_data = $assets[0];
 
-        // get the latest issuance to add the transaction hash
-        $issuances = $this->xcpd_client->get_issuances([
-            'filters' => [
-                ['field' => 'asset',  'op' => '==', 'value' => $asset_name,],
-                ['field' => 'status', 'op' => '==', 'value' => 'valid',],
-            ],
-            'order_by' => 'tx_index',
-            'order_dir' => 'DESC',
-            'limit' => 1,
-        ]);
-        $issuance = $issuances ? $issuances[0] : [];
 
-        $asset_info_data['status']      = $issuance ? $issuance['status']      : null;
-        $asset_info_data['tx_hash']     = $issuance ? $issuance['tx_hash']     : null;
-        $asset_info_data['block_index'] = $issuance ? $issuance['block_index'] : null;
+        $asset_info_datas = [];
+        foreach($asset_names as $offset => $asset_name) {
+            $asset_info_data = $assets[$offset];
 
-        return $asset_info_data;
+            // get the latest issuance to add the transaction hash
+            $issuances = $this->xcpd_client->get_issuances([
+                'filters' => [
+                    ['field' => 'asset',  'op' => '==', 'value' => $asset_name,],
+                    ['field' => 'status', 'op' => '==', 'value' => 'valid',],
+                ],
+                'order_by' => 'tx_index',
+                'order_dir' => 'DESC',
+                'limit' => 1,
+            ]);
+            $issuance = $issuances ? $issuances[0] : [];
+
+            $asset_info_data['status']      = $issuance ? $issuance['status']      : null;
+            $asset_info_data['tx_hash']     = $issuance ? $issuance['tx_hash']     : null;
+            $asset_info_data['block_index'] = $issuance ? $issuance['block_index'] : null;
+
+            $asset_info_datas[] = $asset_info_data;
+        }
+
+
+        return $requested_single_asset ? $asset_info_datas[0] : $asset_info_datas;
     }
 
 
